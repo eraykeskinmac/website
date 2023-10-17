7fedee55270514dfc15fafc37e7afa23ffaed503
Eray Keskin <meraykeskin@gmail.com>
2023-10-17 11:58:51 +0300

    add new matrix gif and constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,22 @@
+export const TRTimeFormatter = new Intl.DateTimeFormat(undefined, {
+	timeZone: 'Europe/Turkey',
+	hour: 'numeric',
+	minute: 'numeric',
+	hour12: false,
+});
+
+export const RelativeTimeFormatter = new Intl.RelativeTimeFormat('en', {
+	style: 'long',
+});
+
+export const discordId = '310839361757511688';
+export const dob = new Date('2004-11-22');
+export const age = new Date(Date.now() - dob.getTime()).getUTCFullYear() - 1970;
+export const hasHadBirthdayThisYear = new Date().getMonth() >= dob.getMonth() && new Date().getDate() >= dob.getDate();
+export const nextBirthdayYear = new Date().getFullYear() + (hasHadBirthdayThisYear ? 1 : 0);
+export const daysUntilBirthday = RelativeTimeFormatter.formatToParts(
+	Math.floor(
+		(new Date(nextBirthdayYear, dob.getMonth(), dob.getDay() + 1).getTime() - Date.now()) / 1000 / 60 / 60 / 24,
+	),
+	'day',
+)[1]!.value.toString();
